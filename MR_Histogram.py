9c2224f4c965d7c6e690109c219512b10d1b2c09
Hu-nie <dhshin8227@kyonggi.ac.kr>
2021-12-17 17:38:21 +0900

    multi-processing

--- MR_Histogram.py
+++ MR_Histogram.py
@@ -1,35 +1,35 @@
-import pandas as pd
-
 from sys import path
 import numpy as np
-import cv2, pydicom
-from numpy.lib.utils import who
 from numpy.core.fromnumeric import shape, size
-import matplotlib.pyplot as plt
-import matplotlib.widgets as widgets
 import SimpleITK as sitk
 import os
+import time
 
-# path = 'D:\\samsung\\red\\'
-# file_list = os.listdir(path)
-# whole_array = np.expand_dims(np.empty((880,880)),axis=0)
+def dicometoarray():
+    path = 'D:\\samsung\\red\\'
+    file_list = os.listdir(path)
+    whole_array = np.expand_dims(np.empty((880,880)),axis=0)
 
 
-# for i in file_list:
-#     try:
-#         dicome_list =  os.listdir(path + i + '/Intracranial TOF source')
-#         for j in dicome_list:
-#            image = sitk.ReadImage(path + i + '/Intracranial TOF source/'+j)
-#            image_array = sitk.GetArrayFromImage(image).astype('float32')
-#            whole_array = np.concatenate((whole_array,image_array),axis=0)
-           
-#         print(j)
-#         print(shape(image_array))
-#     except FileNotFoundError:
-#         pass
-#     except ValueError:
-#         pass
-# whole_array = whole_array[1:]
+
+    for i in file_list:
+
+        try:
+            dicome_list =  os.listdir(path + i + '/Intracranial TOF source')
+            for j in dicome_list:
+               image = sitk.ReadImage(path + i + '/Intracranial TOF source/'+j)
+               image_array = sitk.GetArrayFromImage(image).astype('float32')
+               whole_array = np.concatenate((whole_array,image_array),axis=0)
+
+            print(j)
+            print(shape(image_array))
+        except FileNotFoundError:
+            pass
+        except ValueError:
+            pass
+
+        
+    return whole_array[1:]
 
 
 # Z = (whole_array - whole_array.mean()) / whole_array.std()
@@ -56,29 +56,29 @@ import os
 ##single file analysis
 
 
-path = './intracranial tof/'
-file_list = os.listdir(path)
+# path = './intracranial tof/'
+# file_list = os.listdir(path)
 
-whole_array = np.expand_dims(np.empty((512,512)),axis=0)
+# whole_array = np.expand_dims(np.empty((512,512)),axis=0)
 
-print(shape(whole_array))
+# print(shape(whole_array))
 
-for i in file_list:
-    print(i)
-    image = sitk.ReadImage(path + i)
-    image_array = sitk.GetArrayFromImage(image).astype('float64')
-    # print(shape(image_array))
-    # print(shape(image_array))
-    # image_array = np.expand_dims(image_array,axis=0)
+# for i in file_list:
+#     print(i)
+#     image = sitk.ReadImage(path + i)
+#     image_array = sitk.GetArrayFromImage(image).astype('float64')
+#     # print(shape(image_array))
+#     # print(shape(image_array))
+#     # image_array = np.expand_dims(image_array,axis=0)
 
-    whole_array = np.concatenate((whole_array,image_array),axis=0)
+#     whole_array = np.concatenate((whole_array,image_array),axis=0)
     
-whole_array = whole_array[1:]
+# whole_array = whole_array[1:]
 
-Z = (whole_array - whole_array.mean()) / whole_array.std()
-Z = Z.flatten()
-Z= np.sort(Z)
-print(shape(Z))
+# Z = (whole_array - whole_array.mean()) / whole_array.std()
+# Z = Z.flatten()
+# Z= np.sort(Z)
+# print(shape(Z))
 
 
 # min = np.min(Z)
@@ -86,23 +86,23 @@ print(shape(Z))
 # print(min,max)
 
 
-#Create Graph
-plt.hist(Z, bins =500, histtype='barstacked')
+# #Create Graph
+# plt.hist(Z, bins =500, histtype='barstacked')
 
 
 
 
-# plt.axvline(Z.mean(), color='red',label = 'line at x ={:.3f}'.format(Z.mean()), linestyle='dashed', linewidth=1)
-# plt.axvline(1, color='black',label = 'line at x ={:.3f}'.format(1), linestyle='dashed', linewidth=1)
-# plt.axvline(2, color='yellow',label = 'line at x ={:.3f}'.format(2), linestyle='dashed', linewidth=1)
-# plt.axvline(2.5, color='black',label = 'line at x ={:.3f}'.format(2.5), linestyle='dashed', linewidth=1)
-plt.axvline(3.04, color='blue',label = 'line at x ={:.3f}'.format(3.04), linestyle='dashed', linewidth=1)
-# plt.axvline(4, color='yellow',label = 'line at x ={:.3f}'.format(4), linestyle='dashed', linewidth=1)
+# # plt.axvline(Z.mean(), color='red',label = 'line at x ={:.3f}'.format(Z.mean()), linestyle='dashed', linewidth=1)
+# # plt.axvline(1, color='black',label = 'line at x ={:.3f}'.format(1), linestyle='dashed', linewidth=1)
+# # plt.axvline(2, color='yellow',label = 'line at x ={:.3f}'.format(2), linestyle='dashed', linewidth=1)
+# # plt.axvline(2.5, color='black',label = 'line at x ={:.3f}'.format(2.5), linestyle='dashed', linewidth=1)
+# plt.axvline(3.04, color='blue',label = 'line at x ={:.3f}'.format(3.04), linestyle='dashed', linewidth=1)
+# # plt.axvline(4, color='yellow',label = 'line at x ={:.3f}'.format(4), linestyle='dashed', linewidth=1)
 
 
-plt.grid()
-plt.legend()
-plt.show()
+# plt.grid()
+# plt.legend()
+# plt.show()
 
 
 
