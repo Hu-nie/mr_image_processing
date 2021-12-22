aabdb3a7166b8382936f8ea1763783ad75b660f7
Hu-nie <dhshin8227@kyonggi.ac.kr>
2021-12-22 11:07:40 +0900

    add

--- xg.py
+++ xg.py
@@ -1,5 +1,18 @@
 from sklearn.metrics import r2_score,mean_absolute_error,mean_squared_error, mean_squared_log_error
-
+import pandas as pd
+import numpy as np
 from xgboost import XGBRegressor
+df = pd.read_csv("test.csv")
 
-xgb_r.fit
+
+X = df["x"]
+y = df["y"]
+
+X = X.values
+y = y.values
+
+xgb_r = XGBRegressor(random_state=200)
+
+xgb_r.fit(X,np.log(y))
+
+y_pred_sgv = xgb_r.predict()
